4782ced2153f3ab9a98f4a693345cb0e384ce3f2
TheSkamer <48554750+TheSkamer@users.noreply.github.com>
2020-11-15 21:11:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
- var nameofturned = new Array(
- 					"Horblianskyi Mykhailo && Horaichuk Daryna",
- 					"Olasiuk Anastasiia && Demchyk Nazar",
- 					"Pasechnyk Dmitriy && Maria Farion",
- 					"Vietrova Viktoriia && Nahaievska Marharyta",
- 					"Ostap Bondar && Bohdan Lisovski",
- 					"Dzień jest wolny!"
- );
+ let nameofturned = new Array(
+ 					"Olasiuk Anastasiia",
+ 					"Demchyk Nazar",
+ 					"Vietrova Viktoriia",
+ 					"Nahaievska Marharyta",
+ 					"Maria Farion",
+ 					"Horblianskyi Mykhailo",
+ 					"Horaichuk Daryna",
+ 					"Ostap Bondar",
+ 					"Bohdan Lisovski",
+ 					 );
 /*  var nameofturned = new Array("Mikhailov Dmytro && Krasun Vadym",
 	 				"Oleksandr Serhieiev && Artem Kozak",
  					"Olasiuk Anastasiia && Demchyk Nazar",
@@ -17,32 +20,13 @@
  					"Rudyk Ivanka && Horaichuk Daryna",
  					"Zahaniakha Daria && Nadia Tsupryk",
  					"Dzień jest wolny!");*/
-	var dz = document.getElementById("currentShift");
-	var today = new Date();
-	var day = today.getDate();
-				if(day == 1 || day == 11 || day == 21 ){
-		dz.innerHTML = nameofturned[0];
-	}else if(day == 2 || day == 12 || day == 22 ){
-		dz.innerHTML = nameofturned[1];
-	}else if(day == 3 || day == 13 || day == 23 ){
-		dz.innerHTML = nameofturned[2];
-	}else if(day == 4 || day == 14 || day == 24 ){
-		dz.innerHTML = nameofturned[3];
-	}else if(day == 5 || day == 15 || day == 25 ){
-		dz.innerHTML = nameofturned[4];
-	}else if(day == 6 || day == 16 || day == 26 ){
-		dz.innerHTML = nameofturned[0];
-	}else if(day == 7 || day == 17 || day == 27 ){
-		dz.innerHTML = nameofturned[1];
-	}else if(day == 8 || day == 18 || day == 28 ){
-		dz.innerHTML = nameofturned[2];
-	}else if(day == 9 || day == 19 || day == 29 ){
-		dz.innerHTML = nameofturned[3];
-	}else if(day == 10 || day == 20 | day == 30 ){
-		dz.innerHTML = nameofturned[4];
-	}else{
-		dz.innerHTML = nameofturned[5];
-	}
+	var dz = document.getElementById("currentShift");	
+	let i = 0 ;
+let bubu = setInterval( function() {
+  let x = nameofturned[i++] || clearInterval( bubu );
+  dz.innerHTML = x;
+if(i == 9)
+i = 0;}, 10000);
 /*	if(day == 1 || day == 11 || day == 21 ){
 		dz.innerHTML = nameofturned[0];
 	}else if(day == 2 || day == 12 || day == 22 ){
